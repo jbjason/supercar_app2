013b7bfef7f8736384d2fae6cf91c36d2b81092b
Jubayer <62088928+jbjason@users.noreply.github.com>
2023-04-30 23:51:50 +0600

    initializing

---
+++ lib/task_3/constants/constants3.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const backColor = Color(0xFFF8F8F8);
+const secondaryColor = Color(0xFFF05A76);
+const cardPrimaryColor = Color(0xFFFFFFFF);
+const cardSecondaryColor = Color(0xFFEEEEEE);
+
+const textPrimaryColor = Colors.black;
+const textSecondaryColor = Colors.black38;
